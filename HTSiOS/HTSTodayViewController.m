2a0ce546e53884fb847c011d63578464bf6cc285
Justin Marrington <justin@marrington.net>
2012-05-21 00:06:10 +1000

    Updated Today View Controller to load today's trips via Fetch requests

--- HTSiOS/HTSTodayViewController.m
+++ HTSiOS/HTSTodayViewController.m
@@ -8,12 +8,14 @@
 
 #import "HTSTodayViewController.h"
 #import "Trip.h"
+#import "GeoSample.h"
 
 @interface HTSTodayViewController ()
 
 // Data objects
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, strong) NSDictionary *transportDescriptions;
 
 // Outlets
 @property (nonatomic, weak) IBOutlet MKMapView *mapView;
@@ -28,10 +30,11 @@
 @synthesize fetchedResultsController = _fetchedResultsController;
 @synthesize mapView = _mapView;
 @synthesize tripNameLabel = _tripNameLabel;
+@synthesize transportDescriptions;
 
 - (void)awakeFromNib
 {
-    
+    self.transportDescriptions = [NSDictionary dictionaryWithObjectsAndKeys:@"by car", @"C", @"on foot", @"P", @"cycling", @"Cy", @"on public transport", @"PT", @"by taxi", @"T", nil];
 }
 
 - (void)viewDidLoad
@@ -39,15 +42,16 @@
     [super viewDidLoad];
 
     // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.navigationItem.leftBarButtonItem = [self editButtonItem];
 }
 
 - (void)viewDidUnload
 {
     [self setTripNameLabel:nil];
+    [self setMapView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -62,14 +66,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return [[[self fetchedResultsController] sections] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
     id<NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
     return [sectionInfo numberOfObjects];
@@ -82,61 +84,44 @@
     
     // Configure the cell...
     Trip *trip = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    cell.textLabel = [NSString stringWithFormat:@"%d minute trip %@"];
+    NSDate *start = [trip.samplesSet valueForKeyPath:@"@min.timestamp"];
+    NSDate *end = [trip.samplesSet valueForKeyPath:@"@max.timestamp"];
+    NSTimeInterval length = [end timeIntervalSinceDate:start] / 60;
+    cell.textLabel.text = [NSString stringWithFormat:@"%g minute trip %@", length, [self.transportDescriptions objectForKey:trip.transportType]];
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"hh:mm"];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ — %@: %@", [df stringFromDate:start], [df stringFromDate:end], trip.tripDescription];
+    
     return cell;
 }
 
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
+        NSManagedObjectContext *context = [NSManagedObjectContext MR_defaultContext];
+        Trip *t = [self.fetchedResultsController objectAtIndexPath:indexPath];
+        for (GeoSample *sample in t.samplesSet) {
+            [context deleteObject:sample];
+        }
+        [context deleteObject:t];
+        [context MR_save];
+        
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }   
     else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-}
 
 #pragma mark Auto-intanstiating accessors
 - (CLLocationManager *)locationManager
@@ -164,12 +149,12 @@
         [components setMinute:59];
         [components setSecond:59];
         NSDate *end = [cal dateFromComponents:components];
-        NSPredicate *predicateTemplate = [NSPredicate predicateWithFormat:@"date >= $DATE_START AND date <= $DATE_END"];
-        [predicateTemplate predicateWithSubstitutionVariables:[NSDictionary dictionaryWithObjectsAndKeys:start, @"DATE_START", end, @"DATE_END", nil]];
+        NSPredicate *predicateTemplate = [NSPredicate predicateWithFormat:@"(date >= %@) AND (date <= %@)", start, end];
+        //[predicateTemplate predicateWithSubstitutionVariables:[NSDictionary dictionaryWithObjectsAndKeys:start, @"DATE_START", end, @"DATE_END", nil]];
         [fetch setPredicate:predicateTemplate];
-        [fetch setSortDescriptors:[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"date" ascending:NO]];
+        [fetch setSortDescriptors:[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"date" ascending:NO]]];
         _fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetch managedObjectContext:[NSManagedObjectContext MR_defaultContext]  sectionNameKeyPath:nil cacheName:@"Trip Cache"];
-        _fetchedResultsController performFetch:nil];
+        [_fetchedResultsController performFetch:nil];
     }
     
     return _fetchedResultsController;
