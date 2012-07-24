91c0a0bacf33d41f6aee47b41d9cc6905f9240e8
Justin Marrington <justin@marrington.net>
2012-07-24 12:14:19 +1000

    Fixed Registration view's scrolling (and added closing keyboard by tapping background).

--- HTSiOS/HTSRegisterViewController.m
+++ HTSiOS/HTSRegisterViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *repeatPassword;
 @property (weak, nonatomic) IBOutlet UITextField *email;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) UITextField *activeField;
 @end
 
 @implementation HTSRegisterViewController
@@ -23,23 +24,26 @@
 @synthesize repeatPassword;
 @synthesize email;
 @synthesize scrollView;
+@synthesize activeField;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.scrollView.contentSize = CGSizeMake(320.0, 294.0);
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard:)]];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
 
@@ -70,29 +74,60 @@
 }
 
 #pragma mark Keyboard methods
-- (void)keyboardWillShow:(NSNotification *)not
+- (void)hideKeyboard:(id)sel
 {
-    [self moveScrollViewForKeyboard:not up:YES];
+    [self.activeField resignFirstResponder];
+}
+
+- (void)keyboardDidShow:(NSNotification *)not
+{
+    NSDictionary* info = [not userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
+    
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your application might not need or want this behavior.
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    if (!CGRectContainsPoint(aRect, activeField.frame.origin) ) {
+        CGPoint scrollPoint = CGPointMake(0.0, activeField.frame.origin.y-kbSize.height);
+        [scrollView setContentOffset:scrollPoint animated:YES];
+    }
 }
 
 - (void)keyboardWillHide:(NSNotification *)not
 {
-    [self moveScrollViewForKeyboard:not up:NO];
-}
-
-- (void)moveScrollViewForKeyboard:(NSNotification *)not up:(BOOL)up
-{
-    NSDictionary *info = [not userInfo];
-    
-    
-    CGRect keyboardEndFrame;
-    [[info objectForKey:UIKeyboardFrameEndUserInfoKey] getValue:&keyboardEndFrame];
-    CGRect keyboardFrame = [self.view convertRect:keyboardEndFrame toView:nil];
-    CGFloat scrollDistance = keyboardFrame.size.height * (up ? 1 : -1);
-    [self.scrollView setContentOffset:CGPointMake(0.0, scrollDistance) animated:YES];
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
 }
 
 #pragma mark UITextFieldDelegate methods
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.activeField = textField;
+}
 
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.activeField = nil;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSInteger nextTag = textField.tag + 1;
+    UIResponder *nextResponder = [[textField superview] viewWithTag:nextTag];
+    if (nextResponder) {
+        [nextResponder becomeFirstResponder];
+    } else {
+        [textField resignFirstResponder];
+    }
+    
+    return NO;
+}
 
 @end
